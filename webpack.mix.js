357ed50751009696ca7760c3c9b893e2b7541321
ryu1022 <longzhiliangtaitian@gmail.com>
2023-05-28 14:28:31 +0900

    コード設定の修正

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,27 +1,24 @@
 const mix = require('laravel-mix');
 const { VueLoaderPlugin } = require('vue-loader');
 
+mix.webpackConfig({
+    module: {
+        rules: [
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader'
+            }
+        ]
+    },
+    plugins: [
+        new VueLoaderPlugin()
+    ]
+});
+
+
 mix.js("resources/js/app.js", "public/js")
   .postCss("resources/css/app.css", "public/css", [
     require("tailwindcss"),
+  ])
   
-  .webpackConfig({
-    module: {
-      rules: [
-        {
-          test: /\.vue$/,
-          loader: 'vue-loader'
-        }
-      ]
-    },
-    
-    plugins: [
-      new VueLoaderPlugin()
-    ]
-    
-  ]);
-
-mix.js("resources/js/app.js", "public/js")
-   .postCss("resources/css/app.css", "public/css", [
-      require("tailwindcss"),
-   ]);
+  
